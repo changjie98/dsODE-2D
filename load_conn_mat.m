bbdefb93f396eca127a3541f2b8e1995804d6221
changjie <51906613+changjie98@users.noreply.github.com>
2026-03-10 09:02:02 +0800

    Update load_conn_mat.m

--- load_conn_mat.m
+++ load_conn_mat.m
@@ -11,7 +11,7 @@ if flag == 1
     load(['D:\matlab project\conn_mat\II_sig', num2str(sigII), '_conn_mat.mat'])
     load('D:\matlab project\res_dsODE\positionandmap.mat')
     connection_mat = [EE_conn_mat EI_conn_mat;IE_conn_mat II_conn_mat];
-elif flag == 2
+elseif flag == 2
     load(['D:\matlab project\conn_mat\EE_sig', num2str(sigEE), '_block_conn_mat.mat'])
     load(['D:\matlab project\conn_mat\EI_sig', num2str(sigEI), '_block_conn_mat.mat'])
     load(['D:\matlab project\conn_mat\IE_sig', num2str(sigIE), '_block_conn_mat.mat'])
