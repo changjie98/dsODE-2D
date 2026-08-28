863038d3296b2b7544a6c59c2f39101d70454986
changjie <51906613+changjie98@users.noreply.github.com>
2026-08-28 14:50:33 +0800

    Update main_LIF.m

--- main_LIF.m
+++ main_LIF.m
@@ -8,38 +8,59 @@ function res_lif = main_LIF()
 % means I->E throughout this package.
 
 params = struct();
-params.Ex_Poisson_lambda = 5;
+params.Ex_Poisson_lambda = 7;
 params.M = 100;
 params.Mr = 66;
 params.ne = 300;
 params.ni = 100;
 params.dt = 0.1;
 params.duration_time = 1000;
-params.tau_m = 20;
-params.tau_ee = 3;
-params.tau_ei = 3;
-params.tau_i = 10;
-params.tau_r = 2;
-params.s_ee = 3;
-params.s_ei = 4;
-params.s_ie = 8;
-params.s_ii = 8;
-params.p_ee = 0.2;
-params.p_ei = 0.2;
-params.p_ie = 0.2;
-params.p_ii = 0.2;
+
+% params.tau_m = 20;
+% params.tau_ee = 3;
+% params.tau_ei = 3;
+% params.tau_i = 10;
+% params.tau_r = 2;
+% params.s_ee = 3;
+% params.s_ei = 4;
+% params.s_ie = 8;
+% params.s_ii = 8;
+% params.p_ee = 0.2;
+% params.p_ei = 0.2;
+% params.p_ie = 0.2;
+% params.p_ii = 0.2;
+
+params.tau_m = 0;
+params.tau_ee = 1.4;
+params.tau_ei = 1.2;
+params.tau_i = 4.5;
+params.tau_r = 4;
+params.s_ee = 0.95;
+params.s_ei = 1.25;
+params.s_ie = 2.71;
+params.s_ii = 2.45;
+params.p_ee = 0.8;
+params.p_ei = 0.8;
+params.p_ie = 0.8;
+params.p_ii = 0.8;
+params.connect = 'nofixed'; % 'fixed' or 'nofixed'
 params.record_interval = 5;
-params.refractory_mode = 'fixed';
-params.rng_seed = 1;
+params.refractory_mode = 'exponential';
+%params.refractory_mode = 'fixed';
+params.init_v_min = 0;
+params.init_v_max = 0;
+params.rng_seed = randi(1000);
 
 rng(params.rng_seed,'twister');
-conn_ee = sprand(params.ne,params.ne,params.p_ee) > 0;
-conn_ei = sprand(params.ne,params.ni,params.p_ei) > 0;
-conn_ie = sprand(params.ni,params.ne,params.p_ie) > 0;
-conn_ii = sprand(params.ni,params.ni,params.p_ii) > 0;
-conn_ee(1:params.ne+1:end) = 0;
-conn_ii(1:params.ni+1:end) = 0;
-connection_mat = [conn_ee,conn_ei;conn_ie,conn_ii];
+if strcmp(params.connect,'fixed')
+    conn_ee = sprand(params.ne,params.ne,params.p_ee) > 0;
+    conn_ei = sprand(params.ne,params.ni,params.p_ei) > 0;
+    conn_ie = sprand(params.ni,params.ne,params.p_ie) > 0;
+    conn_ii = sprand(params.ni,params.ni,params.p_ii) > 0;
+    connection_mat = [conn_ee,conn_ei;conn_ie,conn_ii];
+else
+    connection_mat = [];
+end
 
 res_lif = run_LIF_model_core(params,connection_mat);
 res_lif = rmfield(res_lif,'connection_mat');
